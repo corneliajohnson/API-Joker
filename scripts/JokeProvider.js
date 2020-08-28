158b99c4d1fb99a1b28a17f79f58f23d51c915e4
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-28 12:42:37 -0400

    added jokes to dom

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -1,5 +1,14 @@
 let joke;
 
 // 1. define and export a useJoke function that simply returns the joke object.
+export const useJoke = () => {
+  return joke;
+};
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
+
+export const getJoke = () => {
+  return fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => (joke = data));
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,16 @@
 const jokeBtn = document.querySelector(".request-joke-btn");
+import { getJoke, useJoke } from "./JokeProvider.js";
+import { Joke } from "./Joke.js";
+
+const jokeSetupUI = document.querySelector(".joke-setup");
+const jokePunchlineUI = document.querySelector(".joke-punchline");
 
 jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
+  getJoke().then((responseData) => {
+    const jokeStingObj = Joke(responseData);
+    const randomJoke = useJoke(jokeStingObj);
+    jokeSetupUI.innerHTML = `Setup: ${randomJoke.setup}`;
+    jokePunchlineUI.innerHTML = `Puncheline: ${randomJoke.punchline}`;
+  });
 });
